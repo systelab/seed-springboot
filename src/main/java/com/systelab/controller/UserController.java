b8ee5eca72132cf1a0b5f801b75e28ac9313b8b7
alfonsserra <alfonsoserra@gmail.com>
2018-05-30 19:56:05 +0200

    User REST refactor

--- src/main/java/com/systelab/controller/UserController.java
+++ src/main/java/com/systelab/controller/UserController.java
@@ -1,22 +1,24 @@
 package com.systelab.controller;
 
 import com.systelab.model.user.User;
-import com.systelab.model.user.UserRole;
-import com.systelab.repository.PatientNotFoundException;
 import com.systelab.repository.UserNotFoundException;
 import com.systelab.repository.UserRepository;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import javax.annotation.security.PermitAll;
-import javax.servlet.ServletContext;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
+import java.net.URI;
 
 @Api(value = "User", description = "API for user management", tags = {"User"})
 @RestController
@@ -27,11 +29,8 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-
-
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
 
     @ApiOperation(value = "User Login", notes = "")
     @PostMapping(value = "users/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -39,45 +38,35 @@ public class UserController {
     public ResponseEntity authenticateUser(@RequestParam("login") String login, @RequestParam("password") String password) throws SecurityException {
 
         User user = userRepository.findByLoginAndPassword(login, bCryptPasswordEncoder.encode(password));
-
-        if (login.equals("a") && password.equals("a")) {
-            user = new User();
-            user.setLogin("a");
-            user.setPassword("a");
-            user.setName("a");
-            user.setSurname("a");
-            user.setId(1L);
-            user.setRole(UserRole.USER);
-        }
         return ResponseEntity.ok().build();
     }
 
     @ApiOperation(value = "Get all Users", notes = "", authorizations = {@Authorization(value = "Bearer")})
     @GetMapping("users")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @ApiOperation(value = "Create a User", notes = "", authorizations = {@Authorization(value = "Bearer")})
-    @PostMapping("users/user")
-    public User createUser(@RequestBody @ApiParam(value = "User", required = true) @Valid User user) {
-        user.setId(null);
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+    public ResponseEntity<Page<User>> getAllUsers(Pageable pageable) {
+        return ResponseEntity.ok(userRepository.findAll(pageable));
     }
 
     @ApiOperation(value = "Get User", notes = "", authorizations = {@Authorization(value = "Bearer")})
     @GetMapping("users/{uid}")
-    public User getUser(@PathVariable("uid") Long userId) {
-        Optional<User> user = userRepository.findById(userId);
-        if (!user.isPresent())
-            throw new UserNotFoundException(userId);
-        return user.get();
+    public ResponseEntity<User> getUser(@PathVariable("uid") Long userId) {
+        return this.userRepository.findById(userId).map(ResponseEntity::ok).orElseThrow(() -> new UserNotFoundException(userId));
+    }
+
+    @ApiOperation(value = "Create a User", notes = "", authorizations = {@Authorization(value = "Bearer")})
+    @PostMapping("users/user")
+    public ResponseEntity<User> createUser(@RequestBody @ApiParam(value = "User", required = true) @Valid User u) {
+        u.setId(null);
+        u.setPassword(bCryptPasswordEncoder.encode(u.getPassword()));
+        User user = this.userRepository.save(u);
+
+        URI uri = MvcUriComponentsBuilder.fromController(getClass()).path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
     @ApiOperation(value = "Delete a User", notes = "", authorizations = {@Authorization(value = "Bearer")})
     @DeleteMapping("users/{uid}")
-    public  ResponseEntity<?> removeUser(@PathVariable("uid") Long userId) {
+    public ResponseEntity<?> removeUser(@PathVariable("uid") Long userId) {
         return this.userRepository.findById(userId)
                 .map(c -> {
                     userRepository.delete(c);

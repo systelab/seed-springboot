17cc4bbd106cc7073f6fd6180b629babeeb4a1c2
vinyulis <vinyulis@gmail.com>
2018-10-23 23:07:12 +0200

    New ModelBase class with UUID, creation and update time. Extend Patient and User from ModelBase class. Remove UUID from Patient and User because is in ModelBase

---
+++ src/main/java/com/systelab/seed/model/ModelBase.java
@@ -0,0 +1,33 @@
+package com.systelab.seed.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@MappedSuperclass
+@Data
+public abstract class ModelBase {
+
+    @Id
+    @ApiModelProperty(notes = "The database generated model ID")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    protected UUID id;
+
+    @CreationTimestamp
+    protected Timestamp creationTime;
+
+    @UpdateTimestamp
+    protected Timestamp updateTime;
+
+}
--- src/main/java/com/systelab/seed/model/patient/Patient.java
+++ src/main/java/com/systelab/seed/model/patient/Patient.java
@@ -1,40 +1,31 @@
 package com.systelab.seed.model.patient;
 
-import io.swagger.annotations.ApiModelProperty;
+import com.systelab.seed.model.ModelBase;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.UUID;
+
 
 @XmlRootElement
-@XmlType(propOrder = {"id", "name", "surname", "email", "dob", "address"})
+@XmlType(propOrder = {"id", "creationTime", "updateTime", "name", "surname", "medicalNumber", "email", "dob", "address"})
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "patient")
-public class Patient implements Serializable {
-
-    @Id
-    @ApiModelProperty(notes = "The database generated patient ID")
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
-    private UUID id;
+public class Patient extends ModelBase implements Serializable {
 
     @Size(min = 1, max = 255)
     private String surname;

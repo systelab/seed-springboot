988472706498df32518e301e81446c27c720a62b
alfonsserra <alfonsoserra@gmail.com>
2018-11-19 22:34:14 +0100

    Add bucket4j for limiting the incoming calls and do not get a DoS

--- src/main/java/com/systelab/seed/App.java
+++ src/main/java/com/systelab/seed/App.java
@@ -2,6 +2,7 @@ package com.systelab.seed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @EnableFeignClients
+@EnableCaching
 @SpringBootApplication
 public class App {
 

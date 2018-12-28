7b1a7799916d0aae6cee50917c1f4cf52a411081
alfonsserra <alfonsoserra@gmail.com>
2018-12-28 20:43:44 +0100

    Bean Validation requires @NotNull and it is not enough. Check https://docs.oracle.com/javaee/7/tutorial/bean-validation001.htm

--- src/main/java/com/systelab/seed/model/patient/Patient.java
+++ src/main/java/com/systelab/seed/model/patient/Patient.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
@@ -21,9 +22,11 @@ import java.util.Date;
 @Table(name = "patient")
 public class Patient extends ModelBase implements Serializable {
 
+    @NotNull
     @Size(min = 1, max = 255)
     private String surname;
 
+    @NotNull
     @Size(min = 1, max = 255)
     private String name;
 

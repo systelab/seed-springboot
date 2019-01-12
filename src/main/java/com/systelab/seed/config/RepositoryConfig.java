adee892661ac6b7ea7d4365f072dc62cb12119cc
alfonsserra <alfonsoserra@gmail.com>
2019-01-12 23:00:37 +0100

    No need to define the bean as is already a component.

--- src/main/java/com/systelab/seed/config/RepositoryConfig.java
+++ src/main/java/com/systelab/seed/config/RepositoryConfig.java
@@ -1,19 +1,13 @@
 package com.systelab.seed.config;
 
-import com.systelab.seed.config.audit.SpringSecurityAuditorAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaAuditing
 @EnableJpaRepositories(basePackages = {"com.systelab.seed.repository"},
         repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class RepositoryConfig {
-
-    @Bean
-    public AuditorAware<String> auditorProvider() {
-        return new SpringSecurityAuditorAware();
-    }
 }

b7319687beb530745e558e510c79f128534bf6cc
Alfons Serra <alfonsoserra@gmail.com>
2019-10-22 14:17:35 +0200

    Add custom health check (#47)
    * Add custom health check
    * Move healthchecks to an specific folder

--- src/main/java/com/systelab/seed/service/PatientMaintenanceService.java
+++ src/main/java/com/systelab/seed/service/PatientMaintenanceService.java
@@ -1,5 +1,6 @@
 package com.systelab.seed.service;
 
+import com.systelab.seed.config.health.PatientMaintenanceServiceHealthIndicator;
 import com.systelab.seed.repository.PatientRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +17,24 @@ public class PatientMaintenanceService {
 
     private final PatientRepository patientRepository;
 
+    private final PatientMaintenanceServiceHealthIndicator healthIndicator;
+
     @Autowired
-    public PatientMaintenanceService(PatientRepository patientRepository) {
+    public PatientMaintenanceService(PatientRepository patientRepository, PatientMaintenanceServiceHealthIndicator healthIndicator) {
         this.patientRepository = patientRepository;
+        this.healthIndicator = healthIndicator;
     }
 
     @Scheduled(cron = "${patient.maintenance.cron.expression}")
     public void schedulePurgeOlderRecordsTask() {
-        this.patientRepository.setActiveForUpdatedBefore(LocalDateTime.now().minusYears(1));
-        logger.info("Patients DB purged!");
+        try {
+            this.patientRepository.setActiveForUpdatedBefore(LocalDateTime.now().minusYears(1));
+            logger.info("Patients DB purged!");
+            healthIndicator.setWorking(true);
+            healthIndicator.setLastExecution(LocalDateTime.now());
+        } catch (Exception ex) {
+            logger.error("Patients DB not purged!", ex);
+            healthIndicator.setWorking(false);
+        }
     }
 }

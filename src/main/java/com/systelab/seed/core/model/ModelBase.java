ad9da77e6336f0c11d95d9bcd91355b0db863a4c
alfonsserra <alfonsoserra@gmail.com>
2022-11-19 20:01:25 +0100

    Update h2 dependency.

--- src/main/java/com/systelab/seed/core/model/ModelBase.java
+++ src/main/java/com/systelab/seed/core/model/ModelBase.java
@@ -25,7 +25,7 @@ public abstract class ModelBase {
     @Schema(description = "Database generated ID")
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @Column(columnDefinition = "uuid")
     protected UUID id;
 
     @CreatedDate

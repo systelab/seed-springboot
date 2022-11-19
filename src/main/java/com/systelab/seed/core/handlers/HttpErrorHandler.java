0611ec01e893d97a8bcc0c55bfd732f464cfb8a4
alfonsserra <alfonsoserra@gmail.com>
2022-11-19 19:38:33 +0100

    Update dependencies.

--- src/main/java/com/systelab/seed/core/handlers/HttpErrorHandler.java
+++ src/main/java/com/systelab/seed/core/handlers/HttpErrorHandler.java
@@ -11,7 +11,6 @@ public class HttpErrorHandler implements ErrorController {
      *
      * @deprecated
      */
-    @Override
     public String getErrorPath() {
         return "";
     }
--- src/main/java/com/systelab/seed/core/security/authentication/service/AppUserDetailsService.java
+++ src/main/java/com/systelab/seed/core/security/authentication/service/AppUserDetailsService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 @Service(value = "userManagementService")
 public class AppUserDetailsService implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String s) {
